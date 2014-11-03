c896c74b829a758047a652c6a14aa7db97ac4073
Wade Sellers <Waderoni@gmail.com>
2014-11-03 13:48:04 -0600

    Initial Commit

---
+++ RideAlong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RideAlong
+//
+//  Created by Wade Sellers on 11/3/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RideAlong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RideAlong
+//
+//  Created by Wade Sellers on 11/3/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
