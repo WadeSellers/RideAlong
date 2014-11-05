84d8237c9104dc4e313e1f9cba451b1c33193025
Wade Sellers <Waderoni@gmail.com>
2014-11-05 13:28:28 -0600

    added and set proper viewcontroller class files to storyboards

---
+++ RideAlong/RidesMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  RidesMainViewController.h
+//  RideAlong
+//
+//  Created by Wade Sellers on 11/5/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RidesMainViewController : UIViewController
+
+@end
