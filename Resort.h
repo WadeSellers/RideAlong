bae4d2a695346732174f7993bef86fce1fdb816e
Wade Sellers <Waderoni@gmail.com>
2014-11-05 15:08:00 -0600

    Added Resort Object

---
+++ Resort.h
@@ -0,0 +1,17 @@
+//
+//  Resort.h
+//  RideAlong
+//
+//  Created by Wade Sellers on 11/5/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface Resort : NSObject
+
+@property NSString *name;
+@property UIImage *resortPhoto;
+
+@end
---
+++ Resort.m
@@ -0,0 +1,13 @@
+//
+//  Resort.m
+//  RideAlong
+//
+//  Created by Wade Sellers on 11/5/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import "Resort.h"
+
+@implementation Resort
+
+@end
