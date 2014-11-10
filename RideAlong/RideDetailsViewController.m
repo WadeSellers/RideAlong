2b8f61c5b7391f385494928a3866e1df245fd7e1
eshklyar <eshklyar@gmail.com>
2014-11-10 14:29:37 -0600

    did something

--- RideAlong/RideDetailsViewController.m
+++ RideAlong/RideDetailsViewController.m
@@ -8,10 +8,12 @@
 
 #import "RideDetailsViewController.h"
 #import <Parse/Parse.h>
+#import "Ride.h"
 
-@interface RideDetailsViewController ()
+@interface RideDetailsViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *commentsTableView;
 @property (weak, nonatomic) IBOutlet UITextView *commentsTextView;
+@property NSMutableArray * commentsMutableArray;
 
 
 
@@ -21,7 +23,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+ [self loadComments];}
+
+//-(void) loadComments{
+//    NSFetchRequest *request = [[NSFetchRequest alloc]initWithEntityName:@"Ride"];
+//    request.sortDescriptors =[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"comment" ascending:YES]];
+//    self.commentsMutableArray = [self.managedObjectContext executeFetchRequest:request error:nil];
+//    [self.tableView reloadData];
+//}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.commentsTableView.count;
+}
+
+-(UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    Ride  *ride = [self.commentsMutableArray objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell"];
+    cell.textLabel.text =ride.name;
+    return cell;
 }
 
 
