2e999a8749ba99ff0c6143a8b4541177ec911755
Wade Sellers <Waderoni@gmail.com>
2014-11-12 10:23:40 -0600

    connected tableview outlet to myProfileVC

--- RideAlong/ProfileViewController.m
+++ RideAlong/ProfileViewController.m
@@ -9,23 +9,100 @@
 #import "ProfileViewController.h"
 #import "RideDetailsViewController.h"
 
-@interface ProfileViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *profileImage;
-@property (weak, nonatomic) IBOutlet UILabel *usernameTitle;
-@property (weak, nonatomic) IBOutlet UITextView *userDescriptionTextView;
+@interface ProfileViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *myRidesTableView;
+
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *barButton;
+@property NSArray *driverArray;
+@property NSArray *passengerArray;
 
 @end
 
 @implementation ProfileViewController
-- (IBAction)onBarButtonPressed:(id)sender {
-    [self performSegueWithIdentifier:@"commentSegue" sender:self];
-}
+
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self retrieveUserRides];
+    [self.myRidesTableView reloadData];
+
+}
+
+#pragma mark - TableViewDelegate Methods
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0)
+    {
+        return [self.driverArray count];
+    }
+    else
+    {
+        return [self.passengerArray count];
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell"];
+
+    if (indexPath.section == 0) {
+        PFObject *ride = [self.driverArray objectAtIndex:indexPath.row];
+        cell.textLabel.text = ride[@"name"];
+    }
+    else
+    {
+        PFObject *ride = [self.passengerArray objectAtIndex:indexPath.row];
+        cell.textLabel.text = ride[@"name"];
+    }
+    return cell;
+}
+
+#pragma mark - Parse Retrieval Methods
+- (void)retrieveUserRides
+{
+    PFQuery *userIsDriverQuery = [PFQuery queryWithClassName:@"Ride"];
+    [userIsDriverQuery whereKey:@"driver" equalTo:[[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationUUIDKey"]];
+    [userIsDriverQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error)
+        {
+            NSLog(@"Error: %@", error.userInfo);
+            self.driverArray = [NSArray array];
+        }
+        else
+        {
+            self.driverArray = objects;
+            //NSLog(@"rides: %@", self.driverArray);
+        }
+    }];
+
+    PFQuery *userIsPassengerQuery = [PFQuery queryWithClassName:@"Ride"];
+    [userIsPassengerQuery whereKey:@"passenger" equalTo:[[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationUUIDKey"]];
+    [userIsPassengerQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error)
+        {
+            NSLog(@"Error: %@", error.userInfo);
+            self.passengerArray = [NSArray array];
+        }
+        else
+        {
+            self.passengerArray = objects;
+            //NSLog(@"rides: %@", self.passengerArray);
+        }
+    }];
+}
+
+
+
+- (IBAction)onBarButtonPressed:(id)sender
+{
+    [self performSegueWithIdentifier:@"commentSegue" sender:self];
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
@@ -35,8 +112,10 @@
         RideDetailsViewController *rideDetailsViewController = segue.destinationViewController;
         rideDetailsViewController.resortObject = self.resortObject;
     }
-
 }
+
+
+
 
 
 @end
--- RideAlong/RideOrDriveViewController.m
+++ RideAlong/RideOrDriveViewController.m
@@ -20,6 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    NSLog(@"%@", [[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationUUIDKey"]);
+
 }
 
 - (void)didReceiveMemoryWarning {
