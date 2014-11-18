4070030d9de2afc2897c15c93d2fc29d1e36d079
eshklyar <eshklyar@gmail.com>
2014-11-18 12:22:37 -0600

    added collection view, date functions

---
+++ RideAlong/FindRideCollectionViewCell.m
@@ -0,0 +1,15 @@
+//
+//  FindRideCollectionViewCell.m
+//  RideAlong
+//
+//  Created by Edik Shklyar on 11/18/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import "FindRideCollectionViewCell.h"
+
+@implementation FindRideCollectionViewCell
+
+- (IBAction)onFindRideButtonPressed:(UIButton *)sender {
+}
+@end
